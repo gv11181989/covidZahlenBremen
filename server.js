38032e3ce1175e1851763c91f8bed4a33db30ed3
EC2 Default User <ec2-user@ip-172-31-8-81.eu-central-1.compute.internal>
2020-10-14 14:09:29 +0000

    worked on

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ axios.get(url)
 // Load the HTML code as a string, which returns a Cheerio instance
 const $ = cheerio.load(response.data)
 
-// We can use the same API as jQuery to get the desired result
-//const txt = $('#wrapperOuter').text()
+
 const table = $("table > tbody > tr").eq(4).find("td").eq(4); 
 console.log(table.text())
-// Output: "This is an example paragraph"
+
 }).catch(function (e) {
   console.log(e);
 });
